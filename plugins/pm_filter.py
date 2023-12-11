d2270b1fff6e0d3bc5564f621d1326709c5cba19
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-12-11 08:23:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1468,9 +1468,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
     else:
         await query.answer("This Is only for premium users.....\n\nIf You Buy Premium\n\nSend Me - /plans", show_alert=True) 
     
-elif query.data == "coct":
-    buttons = [[
-                InlineKeyboardButton('⟸ Bᴀᴄᴋ', callback_data='help')
+    elif query.data == "coct":
+        buttons = [[
+            InlineKeyboardButton('⟸ Bᴀᴄᴋ', callback_data='help')
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
